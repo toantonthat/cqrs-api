d1037fd8177c3659993305e3b701ac1a9189dbaa
gg_toantt <toantt@greenglobal.vn>
2019-01-15 17:26:43 +0700

    first commit

---
+++ src/main/java/com/toantt/cqrs/App.java
@@ -0,0 +1,13 @@
+package com.toantt.cqrs;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
